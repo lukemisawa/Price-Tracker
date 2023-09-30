2677be6e5d29f5c1ecc62db428b6e77b33b6f590
Luke <lukemisawa@yahoo.com>
2023-09-30 12:54:22 -0700

    Initial layout

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Testing testing")
+    res.send("texttt")
   })
 
 app.listen(port, () =>{
     console.log("Server running on port ${port}.");
 })
+
